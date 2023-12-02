2ab5251a9ed91948eb8aed25204fcc0b2390de6b
Hamed Zare <hamedzare2@gmail.com>
2023-12-03 02:25:50 +0330

    make first for Register Form

--- resources/js/view/v1/setting/registerVal/components/listState.vue
+++ resources/js/view/v1/setting/registerVal/components/listState.vue
@@ -3,8 +3,12 @@
         <v-col cols="12">
             <v-label>استان {{state.title}}</v-label>
         </v-col>
-        <v-col v-for="item in coefficient.data" >{{item}}
-            <v-text-field :label="item.news.name" v-model="data[state.id,item.id]"></v-text-field>
+        <v-col cols="12" sm="6" md="4" lg="3" v-for="item in coefficient.data" >
+            <v-text-field :label="item.news.name" v-model="data[`${state.id}_${item.id}_${item.news_id}`]"></v-text-field>
+        </v-col>
+        <v-col cols="12">
+            <v-label>جمع امتیازات</v-label>
+            {{ sumStateVal(state.id) }}
         </v-col>
         <v-col cols="12"><pre>
             {{data[state.id]}}
@@ -31,15 +35,20 @@ const coefficient = ref(props.coefficient);
 const data=ref({})
 watch(()=>props.yearMonth,(newVal,oldVal)=>{
     yearMonth.value = newVal;
+    reset()
     load()
 
 })
 watch(()=>props.coefficient,(newVal,oldVal)=>{
     coefficient.value=newVal
+    reset()
     load()
 })
 function reset(){
-
+    states.value = [];
+    yearMonth.value = props.yearMonth;
+    coefficient.value = props.coefficient;
+    data.value={}
 }
 function load(){
     api.post('register/edit',{coefficient:coefficient.value.title, yearMonth:yearMonth.value});
@@ -49,6 +58,28 @@ function load(){
             console.log(states);
         })
     ;
+}
+
+function sumStateVal(id){
+
+    let sum = 0;
+    Object.entries(data.value).filter(row=>{
+        let [index,val]=row;
+        if (Number.parseInt(index.split('_'))==id)
+            return val;
+    }).forEach(row=>{
+        let [index,val]=row;
+        let [SID, CID, NID] = index.split('_');
+        let c=Object.entries(coefficient.value.data).filter(row=>{
+            let [index, data] = row;
+            // debugger
+            if (data.id==Number.parseInt(CID) && data.news_id==Number.parseInt(NID))
+                return true;
+            return false;
+        })[0];
+        sum += Number.parseInt(val) * Number.parseInt(c[1].coefficient);
+    })
+    return sum;
 }
 onMounted(()=>{
     load();
